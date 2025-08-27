7d550777ccd34b3d8a25db91a87b83a4f4f008eb
Damiano Papi <dpapi68@gmail.com>
2025-08-27 18:03:29 +0200

    feat: remove svg configuration

--- next.config.ts
+++ next.config.ts
@@ -8,14 +8,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  turbopack: {
-    rules: {
-      '*.svg': {
-        as: '*.js',
-        loaders: ['@svgr/webpack'],
-      },
-    },
-  },
 }
 
 export default nextConfig
